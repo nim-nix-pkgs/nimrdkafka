706d2410f8ebd8ecdf92e99613714a998632570c
hugosenari <hugosenari@gmail.com>
2022-03-12 04:31:17 -0300

    chore: re index nimrdkafka

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim wrapper for librdkafka'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nimrdkafka-master".url = "path:./master";
+  inputs."nimrdkafka-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrdkafka-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim wrapper for librdkafka'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimrdkafka-master.flake = false;
+  inputs.src-nimrdkafka-master.owner = "dfdeshom";
+  inputs.src-nimrdkafka-master.ref   = "refs/heads/master";
+  inputs.src-nimrdkafka-master.repo  = "nimrdkafka";
+  inputs.src-nimrdkafka-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimrdkafka-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimrdkafka-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
